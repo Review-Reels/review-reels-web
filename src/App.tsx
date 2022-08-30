d8733fb9d1d420174eb11c6b960cda83189d2c05
Hariprasad K B <hariwin7@gmail.com>
2022-08-30 20:10:44 +0530

    added smartlooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AllRoutes from "./Routes";
 
+import smartlookClient from "smartlook-client";
+
 function App() {
+  useEffect(() => {
+    if (process.env.NODE_ENV === "production")
+      if (process.env.REACT_APP_SMART_LOOKS_KEY)
+        smartlookClient.init(process.env.REACT_APP_SMART_LOOKS_KEY);
+  });
   return (
     <div className="h-full">
       <AllRoutes />
