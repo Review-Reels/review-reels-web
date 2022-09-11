c73531ec4c131b927111660e7d4947d394ad1e5a
Hariprasad K B <hariwin7@gmail.com>
2022-09-11 13:29:26 +0530

    local urls

--- src/constants/ApiUrls.js
+++ src/constants/ApiUrls.js
@@ -5,17 +5,17 @@ export const S3_URL =
 // export const S3_URL =
 //   "https://review-reels-videos.s3.ap-south-1.amazonaws.com/";
 
-// export const WEB_APP_URL =
-//   "http://review-reels-frontend-test.s3-website.ap-south-1.amazonaws.com/";
-export const WEB_APP_URL = "http://localhost:3001/";
+export const WEB_APP_URL =
+  "http://review-reels-frontend-test.s3-website.ap-south-1.amazonaws.com/";
+// export const WEB_APP_URL = "http://localhost:3001/";
 
 //prod
 // export const WEB_APP_URL = "https://review.reviewreels.app/";
 
 // export const BACKEND_URL = "http://3.108.151.164/api/";
 
-// export const BACKEND_URL = "http://test.reviewreels.app/api/";
-export const BACKEND_URL = "http://127.0.0.1:3000/api/";
+export const BACKEND_URL = "https://test.reviewreels.app/api/";
+// export const BACKEND_URL = "http://127.0.0.1:3000/api/";
 
 //prod
 // export const BACKEND_URL = "https://backend.reviewreels.app/api/";
