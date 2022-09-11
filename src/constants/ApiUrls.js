7c3665223b7d04a80440efda17de7b3a5d3640a1
Hariprasad K B <hariwin7@gmail.com>
2022-09-11 22:51:00 +0530

    checking env

--- src/constants/ApiUrls.js
+++ src/constants/ApiUrls.js
@@ -1,12 +1,13 @@
-export const S3_URL =
-  "https://test-reviewreels-videos.s3.ap-south-1.amazonaws.com/";
+export const S3_URL = process.env.REACT_APP_S3_URL;
+
+// "https://test-reviewreels-videos.s3.ap-south-1.amazonaws.com/";
 
 //prod
 // export const S3_URL =
 //   "https://review-reels-videos.s3.ap-south-1.amazonaws.com/";
 
-export const WEB_APP_URL =
-  "http://review-reels-frontend-test.s3-website.ap-south-1.amazonaws.com/";
+export const WEB_APP_URL = process.env.REACT_APP_WEB_APP_URL;
+// "http://review-reels-frontend-test.s3-website.ap-south-1.amazonaws.com/";
 // export const WEB_APP_URL = "http://localhost:3001/";
 
 //prod
@@ -14,7 +15,8 @@ export const WEB_APP_URL =
 
 // export const BACKEND_URL = "http://3.108.151.164/api/";
 
-export const BACKEND_URL = "https://test.reviewreels.app/api/";
+export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+// "https://test.reviewreels.app/api/";
 // export const BACKEND_URL = "http://127.0.0.1:3000/api/";
 
 //prod
